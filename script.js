08df7081f07d83e77faeab9bd748c34631744bd1
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-25 13:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
 
-function SportsCar(make, model, topSpeed) {}
+	get getMakeModel(){
+		return (this.make, this.model);
+	}
+}
+
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed;
+
+	get getTopSpeed(){
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
