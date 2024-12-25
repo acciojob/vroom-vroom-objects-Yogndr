ac8ecfa76c1fbe22e4bebf34abbb39c7960eca19
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-25 14:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,26 @@
 function Car(make, model) {
 	this.make=make;
 	this.model=model;
-
-	get getMakeModel(){
-		return this.make+" "+this.model;
-	}
 }
 
-function SportsCar(make, model, topSpeed) {
-	car.call(this);
-	this.topSpeed=topSpeed;
+Car.prototype.getMakeModel=function () {
+	return `${this.make} ${this.model}`;
+	
+};
 
-	get getTopSpeed(){
-		return this.topSpeed;
-	}
+
+
+function SportsCar(make, model,topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+
+SportsCar.prototype.getTopSpeed=function () {
+	return this.topSpeed;
+	
 }
 
 // Do not change the code below
